886fa52dda75583270010bb57efb1876d558fbe7
jbabcanec <joseph.babz@gmail.com>
2023-08-22 00:30:20 -0400

    Initial commit from Dropbox

---
+++ get_data.py
@@ -0,0 +1,22 @@
+import pokemontcgsdk
+from pokemontcgsdk import Card, RestClient
+import pandas as pd
+
+# Configure the API key
+RestClient.configure('9e843059-9a5d-4306-81ee-f1054d51849a')
+
+# Fetch all cards
+cards_data = []
+cards = Card.all()
+
+# Collecting card details
+for card in cards:
+    card_dict = card.__dict__
+    cards_data.append(card_dict)
+
+def save_to_excel(data, filename="pokemon_card_data2.xlsx"):
+    df = pd.DataFrame(data)
+    df.to_excel(filename, index=False)
+
+
+save_to_excel(cards_data)
